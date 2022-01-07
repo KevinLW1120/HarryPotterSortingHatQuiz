f3fd3d9b572530a3e69f18807f203bddcc5ad715
Kevin Williams <89435326+KevinLW1120@users.noreply.github.com>
2022-01-07 02:01:14 -0600

    Fixed some flaws

--- sortinghat.cpp
+++ sortinghat.cpp
@@ -12,15 +12,19 @@ int main() {
     int max = 0;
     std::string house;
 
-    std::cout << "Sorting Hat Quiz!\n";
+    std::cout << "===============\n";
+    std::cout << "The Sorting Hat\n";
+    std::cout << "===============\n\n";
 
     //Question 1
+    std::cout<< "Question 1 of 4\n\n";
     std::cout << "\nWhen I'm dead, I want to people to remember me as: \n";
     std::cout << "1) The Good\n";
     std::cout << "2) The Great\n";
     std::cout << "3) The Wise\n";
     std::cout << "4) The Bold\n";
 
+    std::cout << "\nEnter your answer (1-4): ";
     std::cin >> answer1;
 
     //Question 1 if/else
@@ -37,10 +41,12 @@ int main() {
     }
 
     //Question 2
+    std::cout<< "\nQuestion 2 of 4\n\n";
     std::cout << "\nDawm or Dusk?\n";
     std::cout << "1) Dawn\n";
     std::cout << "2) Dusk\n";
 
+    std::cout << "\nEnter your answer (1 or 2): ";
     std::cin>> answer2;
 
     //Question 2 if/else
@@ -55,12 +61,14 @@ int main() {
     }
 
     //Question 3
+    std::cout<< "\nQuestion 3 of 4\n\n";
     std::cout<< "\nWhat kind of instument most pleases your ear?\n";
     std::cout << "1) The violin\n";
     std::cout << "2) The trumpet\n";
     std::cout << "3) The piano\n";
     std::cout << "4) The drum\n";
 
+    std::cout << "\nEnter your answer (1-4): ";
     std::cin >> answer3;
 
     //Question 3 if/else
@@ -77,11 +85,15 @@ int main() {
     }
 
     //Question 4
+    std::cout<< "\nQuestion 4 of 4\n\n";
     std::cout << "\nWhich road tempts you most?\n";
     std::cout << "1) The wide, sunny grassy lane\n";
-    std::cout << "1) The narrow, dark, lantern-lit alley\n";
-    std::cout << "1) The twisting, leaf-stream path through woods\n";
-    std::cout << "1) The cobbled street lined (ancient buildings)\n";
+    std::cout << "2) The narrow, dark, lantern-lit alley\n";
+    std::cout << "3) The twisting, leaf-stream path through woods\n";
+    std::cout << "4) The cobbled street lined (ancient buildings)\n";
+
+    std::cout << "\nEnter your answer (1-4): ";
+    std::cin>> answer4;
 
     //Question 4 if/esle
     if (answer4 == 1) {
@@ -116,4 +128,6 @@ int main() {
         max = slytherin;
         house = "Slytherin";
     }
+
+    std::cout<< "\n\n" << house << "!\n";
 }
