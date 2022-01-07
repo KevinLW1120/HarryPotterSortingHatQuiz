f6508c318142fed94a5c6df262ddb5c38e06773d
Kevin Williams <89435326+KevinLW1120@users.noreply.github.com>
2022-01-07 01:05:33 -0600

    Added variables

---
+++ sortinghat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+    int gryffindor = 0;
+    int hufflepuff = 0;
+    int ravenclaw = 0;
+    int slytherin = 0;
+
+    int answer1, answer2, answer3, answer4;
+}
